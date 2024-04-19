015af33bbc6ca24b2aeb48e099544f6f07ca65a1
shvmn <shvmnrwt@gmail.com>
2024-04-19 08:23:33 +0530

    add mage targets for tailwind and templ

--- magefile.go
+++ magefile.go
@@ -10,15 +10,27 @@ import (
 	"github.com/magefile/mage/sh"
 )
 
-func Run() error {
-	mg.Deps(Build)
-	return sh.RunV("./gitstore")
-}
-
 func Build() error {
-	return sh.RunV("go", "build", "-o", "gitstore", "./cmd/main.go")
+	mg.Deps(TailwindBuild)
+	mg.Deps(TemplGenerate)
+	return sh.RunV("go", "build", "-o", "dist/gitstore", "./cmd/main.go")
 }
 
+func TailwindWatch() error {
+	return sh.RunV("tailwindcss", "-i", "web/static/css/input.css", "-o", "web/static/css/styles.css", "--watch")
+}
+
+func TailwindBuild() error {
+	return sh.RunV("tailwindcss", "-i", "web/static/css/input.css", "-o", "web/static/css/styles.css", "--minify")
+}
+
+func TemplWatch() error {
+	return sh.RunV("templ", "generate", "--watch")
+}
+
+func TemplGenerate() error {
+	return sh.RunV("templ", "generate")
+}
 func Clean() {
 	os.RemoveAll("gitstore")
 }
